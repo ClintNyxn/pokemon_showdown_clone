85f604433aaed41c9733f9ae48dd002e0a981d83
Nyxn Clint <168699316+ClintNyxn@users.noreply.github.com>
2026-03-24 03:55:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,88 +1,89 @@
-const sprite1 = document.querySelector(".sprite1");
-const sprite2 = document.querySelector(".sprite2");
+async function getRandPokemon(){
+    random_number = Math.floor(Math.random(1)*493) + 1
+    baseUrl = `https://pokeapi.co/api/v2/pokemon`
 
-const poke_name1 = document.querySelector(".poke_name1");
-const poke_name2 = document.querySelector(".poke_name2");
+    let dataPromise = await fetch(`${baseUrl}/${random_number}`)
+    let pokemon = await dataPromise.json()
+    return pokemon
+}
 
-const type_p1 = document.querySelector(".type1");
-const type_p2 = document.querySelector(".type2");
-const def1 = document.querySelector(".def1");
-const atk1 = document.querySelector(".atk1");
-const speed1 = document.querySelector(".speed1");
-const def2 = document.querySelector(".def2");
-const atk2 = document.querySelector(".atk2");
-const speed2 = document.querySelector(".speed2");
+let user_pokemons = []
+let oppo_pokemons = []
 
-const move1 = document.querySelector(".move1");
-const move2 = document.querySelector(".move2");
-const move3 = document.querySelector(".move3");
-const move4 = document.querySelector(".move4");
+async function main(){
+    // get 6 pokemons on each side 
+    for(let i=0;i<12;i++){
+        if (user_pokemons.length < 6){
+            user_pokemons.push(await getRandPokemon())
+        }else{
+            oppo_pokemons.push(await getRandPokemon())
+        }
+    }
+    
+    // make 0th index dislay pokemon
+    let user_display_pokemon = user_pokemons[0]   
+    let oppo_display_pokemon = oppo_pokemons[0]   
 
-let op_move1 
-let op_move2 
-let op_move3 
-let op_move4 
+    // user display pokmeon -> back_defualt and vice versa
+    const x = document.querySelector(".user_current_pokemon")
+    const y = document.querySelector(".oppo_current_pokemon")
+    x.src = (user_display_pokemon.sprites.other.showdown.back_default)
+    y.src = (oppo_display_pokemon.sprites.other.showdown.front_default)
 
-let ran1 = Math.floor(Math.random() * 721) + 1;
-let ran2 = Math.floor(Math.random() * 721) + 1;
-
-// random_btn.addEventListener('click', ()=>{
-f1 = fetch(`https://pokeapi.co/api/v2/pokemon/${ran1}`);
-f2 = fetch(`https://pokeapi.co/api/v2/pokemon/${ran2}`);
-type_p1.innerHTML = "";
-type_p2.innerHTML = "";
-
-f1.then((res) => {
-  res.json().then((pokemon) => {
-    pokemon.types.forEach((t) => {
-      type_p1.innerHTML += t.type.name + " ";
-      type_p1.classList = t.type.name;
+    // show rest of the pokemon
+    let options = document.querySelectorAll(".userpoke")
+    i = 0
+    options.forEach(x => {
+        x.src = user_pokemons[i].sprites.other.showdown.front_default 
+        i++
     });
-    //
-    // console.log(pokemon)
-    atk1.innerHTML = "atk- " + pokemon.stats[1].base_stat;
-    def1.innerHTML = "def- " + pokemon.stats[2].base_stat;
-    speed1.innerHTML = "spd- " + pokemon.stats[5].base_stat;
-
-    sprite1.src = pokemon.sprites.other.showdown.back_default;
-
-    poke_name1.innerHTML = pokemon.species.name;
-    poke_moves1 = [];
-    for (let i of pokemon.moves) {
-      poke_moves1.push(i.move.name);
-    }
-    four_moves = [move1, move2, move3, move4];
-    for (let i = 0; i < 4; i++) {
-      four_moves[i].innerHTML =
-        poke_moves1[Math.floor(Math.random() * poke_moves1.length)];
-    }
-  });
-}).catch((err) => console.log(err));
-
-f2.then((res) => {
-  res.json().then((pokemon) => {
-    // console.log(pokemon)
-    pokemon.types.forEach((t) => {
-      type_p2.innerHTML += t.type.name + " ";
-      type_p2.classList = t.type.name;
+    
+    // upon click, user_display_pokemon = clicked
+    options.forEach( o => {
+        o.addEventListener('click',(e => {
+            user_display_pokemon = (user_pokemons[Number(o.classList[1])])
+            x.src = (user_display_pokemon.sprites.other.showdown.back_default)
+        }))
     });
+    
+    // get stats and type
+    const every_poke = document.querySelectorAll(".pokemon")  
 
-    speed2.innerHTML = "spd- " + pokemon.stats[5].base_stat;
+    every_poke.forEach(pokemon => {
+        pokemon.addEventListener('mouseenter', ()=>{
+            
+            if(pokemon.classList[0] == "oppo_current_pokemon"){
+                poke = (oppo_display_pokemon)
+            }else if(pokemon.classList[0] == 'user_current_pokemon'){
+                poke = (user_display_pokemon)
+            }else{
+                poke = (user_pokemons[pokemon.classList[1]])
+            }
+            
+            // if(poke.types.length == 1){
+            //     console.log(poke.types[0].type.name)
+            // }else{
+            //     console.log(poke.types[0].type.name)
+            //     console.log(poke.types[1].type.name)
+            // }
+                
 
-    sprite2.src = pokemon.sprites.other.showdown.front_default;
+            poke_name = (poke.name)
+            poke_hp = (poke.stats[0].base_stat)
+            poke_atk = (poke.stats[1].base_stat)
+            poke_def = (poke.stats[2].base_stat)
+            poke_satk = (poke.stats[3].base_stat)
+            poke_sdef = (poke.stats[4].base_stat)
+            poke_speed = (poke.stats[5].base_stat)
 
-    poke_name2.innerHTML = pokemon.species.name;
-
-    let poke_moves2 = [];
-    for (let i of pokemon.moves) {
-      poke_moves2.push(i.move.name);
-    }
-    four_moves = [op_move1, op_move2, op_move3, op_move4];
-    // console.log(four_moves)
-    for (let i = 0; i < 4; i++) {
-      console.log(four_moves[i])
-      four_moves[i].innerHTML = poke_moves2[Math.floor(Math.random() * poke_moves2.length)];
-      // console.log(four_moves)
-    }
-  });
-}).catch((err) => console.log(err));
+            // show stats and type
+            const stat_show = document.querySelector(".stat_show")
+            stat_show.innerHTML = '' 
+            stat_show.innerHTML +=  `name - ${poke_name} `
+            stat_show.innerHTML +=  `atk - ${poke_atk} `
+            stat_show.innerHTML +=  `def - ${poke_def} `
+            stat_show.innerHTML +=  `speed - ${poke_speed} `
+        }) 
+    });
+    
+}main()
